da18968e9d1440ef7a3edb6df3e157d7e0866a86
Craig Loomis <cloomis@astro.princeton.edu>
2012-11-14 12:27:27 -0500

    add mcs keys

--- python/actorkeys/mcs.py
+++ python/actorkeys/mcs.py
@@ -1,4 +1,4 @@
 KeysDictionary('mcs', (1, 1),
                Key("text", String(help="text for humans")),
                Key("version", String(help="EUPS/git version")),
-               Key("centroids", Float(units="mm"))*5000)
+               Key("centroids", Float(units="mm")*(5000)))
