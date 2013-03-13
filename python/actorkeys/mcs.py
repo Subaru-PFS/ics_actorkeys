00cb2ff1557cc7f798d254cf8c609993d3db6d75
Craig Loomis <cloomis@astro.princeton.edu>
2013-03-12 22:21:36 -0400

    define an encapsulated form of the centroids

--- python/actorkeys/mcs.py
+++ python/actorkeys/mcs.py
@@ -1,4 +1,5 @@
-KeysDictionary('mcs', (1, 1),
+KeysDictionary('mcs', (1, 2),
                Key("text", String(help="text for humans")),
                Key("version", String(help="EUPS/git version")),
+               Key("centroidsChunk", String(help="base64 encoded centroids")),
                Key("centroids", Float(units="mm")*(5000)))
