a0f22c21f0df68d8a6d8554cc5a9d0822bce8675
LE FUR Arnaud <arnaud.lefur@lam.fr>
2017-05-17 09:34:17 +0000

    dcbActor added bakeout keyword, vaw for pdu

--- python/actorkeys/dcb.py
+++ python/actorkeys/dcb.py
@@ -1,10 +1,11 @@
-KeysDictionary('dcb', (1, 1),
+KeysDictionary('dcb', (1, 2),
                Key("text", String(help="text for humans")),
                Key("version", String(help="EUPS/git version")),
                Key("ne", Bool("off", "on", name='neon', help='neon lamp switch')),
                Key("xenon", Bool("off", "on", name='xenon', help='xenon lamp switch')),
                Key("hgar", Bool("off", "on", name='hgar', help='Hg-Ar switch')),
                Key("deuterium", Bool("off", "on", name='deuterium', help='deuterium lamp switch')),
+               Key("bakeout", Bool("off", "on", name='bakeout', help='bakeout cover')),
                Key("pow_attenuator", Bool("off", "on", name='pow_attenuator', help='power attenuator switch')),
                Key("pow_sphere", Bool("off", "on", name='pow_sphere', help='power int sphere switch')),
                Key("pow_halogen", Bool("off", "on", name='pow_halogen', help='power halogen switch')),
@@ -12,5 +13,10 @@ KeysDictionary('dcb', (1, 1),
                Key("photodiode", Float(invalid="Nan", help='photo diode flux (foot-lambert)')),
                Key("attenuator", UInt(help='attenuator opening value (close=0)')),
                Key("halogen", Bool("off", "on", name='halogen', help='halogen switch')),
+               Key("aten_vaw",
+                   Float(name='V', help='Voltage'),
+                   Float(name='A', help='Current'),
+                   Float(name='W', help='Power'), help='aten current Volt, Amp, Power')
 
                )
+
