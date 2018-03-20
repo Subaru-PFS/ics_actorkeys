31078160f17ab7a1a1b2b773e6a072e30fb8c46d
Chih-Yi <wency@asiaa.sinica.edu.tw>
2018-03-20 17:31:11 +0800

    Add two new keywords

--- python/actorkeys/meb.py
+++ python/actorkeys/meb.py
@@ -1,7 +1,10 @@
-KeysDictionary('meb', (1, 4),
+KeysDictionary('meb', (1, 5),
                Key("text", String(help="text for humans")),
                Key("version", String(help="EUPS/git version")),
                Key('temps', Float()*7,
-                   help='The MCS Ebox temperatures'),
-               Key('power', Int()*4),
+                   help='MCS Ebox temperatures'),
+               Key('power', Int()*4,
+                   help='MCS power status'),
+               Key('flow', Float(),
+                   help='Flow meter for MCS'),
 )
