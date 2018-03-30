3d3a1a6165b0d87cdb0e4d42da5fb77f83271b2b
Craig Loomis <cloomis@astro.princeton.edu>
2018-03-30 16:37:45 -0400

    add initial gen2 keys

---
+++ python/actorkeys/gen2.py
@@ -0,0 +1,6 @@
+KeysDictionary('gen2', (1, 4),
+               Key("text", String(help="text for humans")),
+               Key("version", String(help="EUPS/git version")),
+               Key("frameid", Int(help="Gen2-assigned frame ID")),
+               Key("header", String(help="Gen2-supplied FITS header")),
+)
