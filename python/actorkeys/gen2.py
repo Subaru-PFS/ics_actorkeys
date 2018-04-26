bbb08d0489457962ab3ce89ef57e10c874f52b45
Craig Loomis <cloomis@shell-ics.pfs.sum.subaru.nao.ac.jp>
2018-04-25 17:06:51 -1000

    frameid -> visit

--- python/actorkeys/gen2.py
+++ python/actorkeys/gen2.py
@@ -1,6 +1,6 @@
-KeysDictionary('gen2', (1, 4),
+KeysDictionary('gen2', (1, 5),
                Key("text", String(help="text for humans")),
                Key("version", String(help="EUPS/git version")),
-               Key("frameid", Int(help="Gen2-assigned frame ID")),
+               Key("visit", Int(help="Gen2-assigned PFS visit")),
                Key("header", String(help="Gen2-supplied FITS header")),
 )
