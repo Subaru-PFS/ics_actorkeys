f81cc47ec8eae094602d9e2162d5c76e3a8fc5a9
Arnaud Le Fur <arnaud.lefur@lam.fr>
2018-05-04 11:56:41 +0200

    add seqno provide visitId

---
+++ python/actorkeys/seqno.py
@@ -0,0 +1,5 @@
+KeysDictionary('seqno', (1, 1),
+               Key("text", String(help="text for humans")),
+               Key("version", String(help="EUPS/git version")),
+               Key("visit", Int(help="assigned PFS visit")),
+)
