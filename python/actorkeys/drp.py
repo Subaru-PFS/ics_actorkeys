021deb1d8566d862bbe81a1c74ed745c376ec885
Arnaud Le Fur <arnaud.lefur@lam.fr>
2018-09-14 15:31:21 +0200

    add drpActor keywords

---
+++ python/actorkeys/drp.py
@@ -0,0 +1,6 @@
+KeysDictionary('drp', (1, 1),
+               Key('text', String(help='text for humans')),
+               Key('ingest', String(name='filepath', help='ingested expoure filepath')),
+               Key('detrend', String(name='filepath',help='detrended expoure filepath'))
+
+               )
