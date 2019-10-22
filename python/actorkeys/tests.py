d03c1dfdceea87ea9f96bb549f6800c66b4321a3
Arnaud Le Fur <alefur@lam.fr>
2019-10-22 13:51:31 +0000

    add tests keywords

---
+++ python/actorkeys/tests.py
@@ -0,0 +1,9 @@
+KeysDictionary('tests', (1, 1),
+               Key("text", String(help="text for humans")),
+               Key('version', String(help="test actor version")),
+               Key("keytest1", Float(invalid="NaN") * (1, 2)),
+               Key("keytest2", Float(invalid="NaN")),
+               Key("keytest3",
+                   Enum("Open", "Closed", name="test"),
+                   String(name="errors", help="human-oriented error string")),
+               )
