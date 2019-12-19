ae1073712af1eda6007aa2a4ca3ceb0e0612f6e0
Arnaud Le Fur <arnaud.lefur@lam.fr>
2019-12-18 16:33:50 -1000

    add rough controllers key

--- python/actorkeys/rough.py
+++ python/actorkeys/rough.py
@@ -1,9 +1,9 @@
-KeysDictionary('rough', (2, 1),
+KeysDictionary('rough', (2, 2),
                Key("text",
                    String(help="text for humans")),
                Key("version",
                    String(help="EUPS/git version")),
-
+               Key("controllers", String(help='controllers list') * (1, None)),
                Key('pumpSpeed',
                    Float(units='rpm',
                    STS=0, help='roughing pump speed')),
