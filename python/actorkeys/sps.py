90c49728eb147be9a44f41d339f5ec8b22be64af
Arnaud Le Fur <arnaud.lefur@lam.fr>
2020-05-25 21:08:25 +0200

    sps fileIds

--- python/actorkeys/sps.py
+++ python/actorkeys/sps.py
@@ -1,11 +1,14 @@
-KeysDictionary('sps', (1, 3),
+KeysDictionary('sps', (1, 4),
                Key('text', String(help='text for humans')),
                Key('version', String(help='SPS actor version',
                                      FITS=('W_RVSPS', 'W_SPSACTOR_VERSION'))),
                Key('version_tron_actorcore', String()),
                Key('version_pfs_utils', String()),
                Key('version_ics_actorkeys', String()),
-               Key('visit', Int(help='assigned PFS visit')),
                Key('exposable', String(help='camList that can be safely exposed') * (1, None)),
+               Key('fileIds',
+                   Int(help='assigned PFS visit'), String('camera list'), UInt(name="camMask", reprFmt="0x%04x")),
+               Key('visit', Int(help='assigned PFS visit')),
                Key('frames', String(help='camList for frames that has genuinely been created') * (1, None)),
+
                )
