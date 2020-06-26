cfba73c7b10348426cfdc1794a069ea2756b7641
Craig Loomis <cloomis@astro.princeton.edu>
2020-06-26 11:26:23 -1000

    Add iic.py model dictionary
    Basically empty, but gives us the iccActor version

---
+++ python/actorkeys/iic.py
@@ -0,0 +1,10 @@
+KeysDictionary('iic', (1,0),
+               Key("controllers", String(help='controllers list') * (1, None)),
+               Key("text", String(), help='Stuff for humans'),
+               Key('version', String(help="IIC actor version",
+                                     FITS=("W_RVIIC",
+                                           "W_IICACTOR_VERSION"))),
+               Key('version_tron_actorcore', String(help="tron_actorcore version")),
+               Key('version_ics_actorkeys', String(help="ics_actorkeys version")),
+               Key('version_pfs_utils', String(help="pfs_utils version")),
+)
