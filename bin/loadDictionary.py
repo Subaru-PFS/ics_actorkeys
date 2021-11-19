692513af5ccdd3fe194262714ff3f4dd60e3a4d6
Arnaud Le Fur <arnaud.lefur@lam.fr>
2021-11-18 16:53:53 -1000

    replace glob.glob with pkgutil

--- bin/loadDictionary.py
+++ bin/loadDictionary.py
@@ -1,37 +1,29 @@
 #!/usr/bin/env python3
 
 import argparse
-import glob
-import os
+import pkgutil
 
+import actorkeys
 from opscore.protocols import keys
 
 
 def gatherAllDictionaries():
     """ Load all existing actorkeys dictionaries"""
-    toCheck = []
-    actorkeysDir = os.environ['ICS_ACTORKEYS_DIR']
-    files = glob.glob(f'{actorkeysDir}/*/*/*.py')
-
-    for file in files:
-        __, actorkeyPy = os.path.split(file)
-        actorkey, __ = os.path.splitext(actorkeyPy)
-        toCheck.append(actorkey)
-
-    return toCheck
+    allDict = [modname for __, modname, __ in pkgutil.iter_modules(actorkeys.__path__)]
+    return allDict
 
 
 def main(argv=None):
-    """Command-line interface to load actorkey dictionary. """
-    parser = argparse.ArgumentParser(description="load actorkey dictionary")
-    parser.add_argument('-l', '--actors', nargs='+',  default=None, help='actorkeys dictionary(ies) to load')
+    """Command-line interface to load actorkeys dictionary. """
+    parser = argparse.ArgumentParser(description="load actorkeys dictionary")
+    parser.add_argument('-l', '--actors', nargs='+', default=None, help='actorkeys dictionary(ies) to load')
 
     args = parser.parse_args(argv)
 
-    toCheck = gatherAllDictionaries() if args.actors is None else args.actors
+    allDict = gatherAllDictionaries() if args.actors is None else args.actors
 
-    for actor in toCheck:
-        keys.KeysDictionary.load(actor, forceReload=True)
+    for dictname in allDict:
+        keys.KeysDictionary.load(dictname, forceReload=True)
 
 
 if __name__ == '__main__':
