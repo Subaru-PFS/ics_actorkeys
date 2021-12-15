b3d5990ad8da9fcaddf2dda7968006b77f2f46bf
Arnaud Le Fur <arnaud.lefur@lam.fr>
2021-12-15 18:52:38 -0500

    add drp command status keys

--- python/actorkeys/drp.py
+++ python/actorkeys/drp.py
@@ -1,6 +1,22 @@
-KeysDictionary('drp', (1, 1),
+KeysDictionary('drp', (1, 2),
                Key('text', String(help='text for humans')),
-               Key('ingest', String(name='filepath', help='ingested expoure filepath')),
-               Key('detrend', String(name='filepath',help='detrended expoure filepath'))
+               Key('ingest', String(name='filepath', help='ingested exposure filepath')),
+               Key('detrend', String(name='filepath', help='detrended exposure filepath')),
+
+               Key('ingestStatus',
+                   Int(name="visit", help="the PFS visit"),
+                   Int(name="returnCode", help="command return code"),
+                   String(name="statusStr", help="command status string"),
+                   Float(name='timing', units="s", help='command timing in seconds')),
+               Key('detrendStatus',
+                   Int(name="visit", help="the PFS visit"),
+                   Int(name="returnCode", help="command return code"),
+                   String(name="statusStr", help="command status string"),
+                   Float(name='timing', units="s", help='command timing in seconds')),
+               Key('reduceExposureStatus',
+                   Int(name="visit", help="the PFS visit"),
+                   Int(name="returnCode", help="command return code"),
+                   String(name="statusStr", help="command status string"),
+                   Float(name='timing', units="s", help='command timing in seconds')),
 
                )
